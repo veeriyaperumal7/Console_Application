68b4c6bea0e12953054e39136628ad2e9962e0be
zoho <zoho@172.17.58.232>
2024-01-13 16:15:42 +0530

    Billing View work

--- src/com/veeriyaperumal/hotelbilling/billing/BillingView.java
+++ src/com/veeriyaperumal/hotelbilling/billing/BillingView.java
@@ -1,12 +1,32 @@
 package com.veeriyaperumal.hotelbilling.billing;
 
 import com.veeriyaperumal.hotelbilling.base.BaseView;
+import com.veeriyaperumal.hotelbilling.model.Bill;
 
-public class BillingView extends BaseView{
+public class BillingView extends BaseView {
+
+	private Bill currentBill = new Bill();
 
 	public void showBillingOptions() {
-       menuOptions.clear();
-       
+		showBillingFeatures();
+	}
+
+	private void showBillingFeatures() {
+		menuOptions.clear();
+		menuOptions.add("Add Dish");
+		menuOptions.add("Finish Bill");
+		menuOptions.add("Cancel Bill");
+		do {
+			printCurrentBill();
+			printOptionsTable(menuOptions, "Billing Options");
+			getIntegerInput("Choose valid options : ", 1, 3);
+		} while (true);
+
+	}
+
+	private void printCurrentBill() {
+	
+		
 	}
 
 }
